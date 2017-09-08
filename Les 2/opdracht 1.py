36c77ab441386787e6d0d893ee146ff222d18078
Mees van der Winden <mees.vanderwinden@student.hu.nl>
2017-09-08 16:07:01 +0200

    les 2

---
+++ Les 2/opdracht 1.py
@@ -0,0 +1,24 @@
+a = 6
+b = 7
+c = (a+b)/2
+print(c)
+
+voorNaam = 'Mees'
+tussenVoegsel = 'van der'
+achterNaam = 'Winden'
+blankSpace = ' '
+
+mijnNaam = voorNaam + blankSpace + tussenVoegsel + blankSpace + achterNaam
+
+print (mijnNaam)
+
+a = 6
+b = 7
+inventaris = ['papier','nietjes','pennen']
+mijnnaam = ['Mees van der Winden']
+print(6.75 > a and 6.75 < b)
+
+print(len(inventaris)>5*len(mijnnaam))
+
+print(len(inventaris)==0 or len (inventaris)>2)
+
